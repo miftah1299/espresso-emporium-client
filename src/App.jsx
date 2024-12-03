de7a56dc4bb8b82ed5546f720dba4b55112617b8
Your Name <jannat27120@gmail.com>
2024-12-03 18:05:19 +0600

    initialize React app with Vite, Tailwind CSS, and ESLint configuration

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+    return (
+        <>
+            <div className="p-6">
+                <h1 className="text-5xl text-blue-500">
+                    this is a react app with vite
+                </h1>
+
+                <button className="btn btn-outline">Click</button>
+            </div>
+        </>
+    );
+}
+
+export default App;
