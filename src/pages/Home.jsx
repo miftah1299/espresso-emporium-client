d74aed85cba8d3f269c8be39d28cab28155cacbd
Your Name <jannat27120@gmail.com>
2024-12-03 22:30:03 +0600

    add coffee card component, integrate SweetAlert for success notifications

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
+import { useLoaderData } from "react-router-dom";
+import CoffeeCard from "../components/CoffeeCard";
 
 const Home = () => {
+    const coffees = useLoaderData();
+
     return (
-        <div>
-            Home Page
+        <div className="text-center space-y-4">
+            <h5>--- SIp & Savor ---</h5>
+            <h2>Our Popular Products</h2>
+            <button className="btn">Add Coffee</button>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                {coffees.map((coffee) => (
+                    <CoffeeCard key={coffee._id} coffee={coffee} />
+                ))}
+            </div>
         </div>
     );
 };
